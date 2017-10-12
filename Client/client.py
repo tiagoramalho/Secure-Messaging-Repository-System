59a3c69a2a1d793fc19f9696e3892eef0b2a3d0a
Tiago Ramalho <t.ramalho22@ua.pt>
2017-10-12 16:51:58 +0100

    identaçao

--- Client/client.py
+++ Client/client.py
@@ -14,328 +14,314 @@ from random import randint
 TERMINATOR = "\r\n"
 
 def get_int(question):
-	try:
-		return int(input(question))
-	except:
-		return None
+    try:
+        return int(input(question))
+    except:
+        return None
 
 
 class Client(object):
-	"""docstring for Client"""
-	def __init__(self, uuid):
-		super(Client, self).__init__()
+    """docstring for Client"""
+    def __init__(self, uuid):
+        super(Client, self).__init__()
 
-		# Server connection variables
-		try:
-			self.server_ip = "127.0.0.1"
-			self.server_port = 8080
-			self.socket = socket(AF_INET, SOCK_STREAM)
-			self.socket.connect((self.server_ip, self.server_port))
-		except Exception as e:
-			log_error("Error connecting with the server")
-			raise
-			return
-
-
-		try:
-			self.uuid = int(uuid)
-		except ValueError:
-			log_error("UUID must be an integer")
-			raise
-			return
-
-		self.id = self.get_self_ID()
-		if not self.id:
-			log_info("Creating message box...")
-			self.Create()
+        # Server connection variables
+        try:
+            self.server_ip = "127.0.0.1"
+            self.server_port = 8080
+            self.socket = socket(AF_INET, SOCK_STREAM)
+            self.socket.connect((self.server_ip, self.server_port))
+        except Exception as e:
+            log_error("Error connecting with the server")
+            raise
+            return
 
 
+        try:
+            self.uuid = int(uuid)
+        except ValueError:
+            log_error("UUID must be an integer")
+            raise
+            return
+
+        self.id = self.get_self_ID()
+        if not self.id:
+            log_info("Creating message box...")
+            self.Create()
 
 
 
-	def get_self_ID(self):
-		"""
-			Returns message box ID for the client UUID
-			Returns None if it doesn't exist
-		"""
-
-		message = {'type' : 'list'}
-		self.send_to_server(message)
-		response = json.loads(self.socket.recv(1024))
-
-		if not response.get('error'):
-			for x in response.get('result'):
-				if self.uuid == int(x.get('description').get('uuid')):
-					log_info("You have a message box already registered (ID: %s)" % x.get('id'))
-					return int(x.get('id'))
 
 
-		log_info("You don't have a message box yet.")
-		return None
+    def get_self_ID(self):
+        """
+            Returns message box ID for the client UUID
+            Returns None if it doesn't exist
+        """
+
+        message = {'type' : 'list'}
+        self.send_to_server(message)
+        response = json.loads(self.socket.recv(1024))
+
+        if not response.get('error'):
+            for x in response.get('result'):
+                if self.uuid == int(x.get('description').get('uuid')):
+                    log_info("You have a message box already registered (ID: %s)" % x.get('id'))
+                    return int(x.get('id'))
+
+
+        log_info("You don't have a message box yet.")
+        return None
 
 
 
-	def send_to_server(self, message):
-		self.socket.sendall(json.dumps(message) + TERMINATOR)
+    def send_to_server(self, message):
+        self.socket.sendall(json.dumps(message) + TERMINATOR)
 
-	def Create(self):
-		message = { 'type' : 'create',
-			   		'uuid' : self.uuid
-			  	  }
+    def Create(self):
+        message = { 'type' : 'create',
+                    'uuid' : self.uuid
+                  }
 
-		self.send_to_server(message)
-		response = json.loads(self.socket.recv(1024))
+        self.send_to_server(message)
+        response = json.loads(self.socket.recv(1024))
 
-		if response.get('error'):
-			log_error("This uuid already has a message box")
+        if response.get('error'):
+            log_error("This uuid already has a message box")
 
-		else:
-			self.id = response.get('result')
-			log_success("Message box with created successfully (ID: %s)" % str(response.get('result')))
-
-
-	def List(self, uid = None):
-		if uid == None:
-			message = {'type' : 'list'}
-		else:
-			message = {'type' : 'list', 'id' : uid}
-
-		self.send_to_server(message)
-		response = json.loads(self.socket.recv(1024))
-
-		if response.get('error'):
-			log_error(response.get('error'))
-		else:
-			print response
-			try:
-				for x in response.get('result'):
-					print x
-			except Exception as e:
-				log_info("Id does not exist")
+        else:
+            self.id = response.get('result')
+            log_success("Message box with created successfully (ID: %s)" % str(response.get('result')))
 
 
-	def New(self, user_id):
-		message = { 'type' : 'new', 
-	   				'id' : user_id
-	  	  		  }
-		
-		self.send_to_server(message)
-		response = json.loads(self.socket.recv(1024))
+    def List(self, uid = None):
+        if uid == None:
+            message = {'type' : 'list'}
+        else:
+            message = {'type' : 'list', 'id' : uid}
 
-		if response.get('error'):
-			log_error(response.get('error'))
+        self.send_to_server(message)
+        response = json.loads(self.socket.recv(1024))
 
-		elif len(response.get('result')) == 0:
-			log_info("No new messages to show")
-
-		else:
-			for x in response.get('result'):
-				print x
-
-
-	def All(self, uid):
-				message = {'type' : 'all', 'id' : uid}
-				self.send_to_server(message)
-				response = json.loads(self.socket.recv(1024))
-				if response.get('error'):
-						log_error(response.get('error'))
-				else:
-						print response
-
-	def Send(self, dst, msg, src = None):
-		src = self.id if src == None else src
-		message = { 'type'	: 'send', 
-	   				'src'	: src,
-	   				'dst'	: dst,
-	   				'msg'	: msg,
-	   				'copy'	: msg
-	  	  		  }
-
-	  	self.send_to_server(message)
-		response = json.loads(self.socket.recv(1024))
-		print response
+        if response.get('error'):
+            log_error(response.get('error'))
+        else:
+            print response
+            try:
+                for x in response.get('result'):
+                    print x
+            except Exception as e:
+                log_info("Id does not exist")
 
 
-	def Recv(self, receiver, box):
-		message = { 'type'	: 'recv', 
-	   				'id'	: receiver,  
-	   				'msg'	: box,
-	  	  		  }
+    def New(self, user_id):
+        message = { 'type' : 'new', 
+                    'id' : user_id
+                  }
+        
+        self.send_to_server(message)
+        response = json.loads(self.socket.recv(1024))
 
-	  	self.send_to_server(message)
-		response = json.loads(self.socket.recv(1024))
-		if response.get('error'):
-			log_error(response.get('error'))
-		else:
-			print response
-			
+        if response.get('error'):
+            log_error(response.get('error'))
 
-	def Receipt(self, box):
-		message = { 'type'		: 'receipt', 
-	   				'id'		: self.id,  
-	   				'msg'		: box,
-	   				'receipt'	: "Vamos imaginar que este texto e uma assinatura"  # hint: Não é ;) 
-	  	  		  }
+        elif len(response.get('result')) == 0:
+            log_info("No new messages to show")
 
-	  	self.send_to_server(message)
-	  	self.socket.settimeout(0.5)
-	  	try:
-			response = json.loads(self.socket.recv(1024))
-			log_error(response.get('error'))
-		except:
-			log_success("Sent")
-	  	self.socket.settimeout(None)
+        else:
+            for x in response.get('result'):
+                print x
 
-		"""
-		if response.get('error'):
-			log_error(response.get('error'))
-		else:
-			print response
 
-		return 
-		"""
+    def All(self, uid):
+        message = {'type' : 'all', 'id' : uid}
+        self.send_to_server(message)
+        response = json.loads(self.socket.recv(1024))
+        if response.get('error'):
+            log_error(response.get('error'))
+        else:
+            print response
 
-	def Status(self, sender, box):
-		message = { 'type'	: 'status', 
-	   				'id'	: sender,  
-	   				'msg'	: box,
-	  	  		  }
+    def Send(self, dst, msg, src = None):
+        src = self.id if src == None else src
+        message = { 'type'	: 'send', 
+                    'src'	: src,
+                    'dst'	: dst,
+                    'msg'	: msg,
+                    'copy'	: msg
+                  }
 
-	  	self.send_to_server(message)
-		response = json.loads(self.socket.recv(1024))
-		if response.get('error'):
-			log_error(response.get('error'))
-		else:
-			print response
-		pass
+        self.send_to_server(message)
+        response = json.loads(self.socket.recv(1024))
+        print response
+
+
+    def Recv(self, receiver, box):
+        message = { 'type'	: 'recv', 
+                    'id'	: receiver,  
+                    'msg'	: box,
+                  }
+
+        self.send_to_server(message)
+        response = json.loads(self.socket.recv(1024))
+        if response.get('error'):
+            log_error(response.get('error'))
+        else:
+            print response
+                
+
+    def Receipt(self, box):
+        message = { 'type'		: 'receipt', 
+                    'id'		: self.id,  
+                    'msg'		: box,
+                    'receipt'	: "Vamos imaginar que este texto e uma assinatura"  # hint: Não é ;) 
+                  }
+
+        self.send_to_server(message)
+        self.socket.settimeout(0.5)
+        try:
+            response = json.loads(self.socket.recv(1024))
+            log_error(response.get('error'))
+        except:
+            log_success("Sent")
+        self.socket.settimeout(None)
+
+
+    def Status(self, sender, box):
+        message = { 'type'	: 'status', 
+                    'id'	: sender,  
+                    'msg'	: box,
+                  }
+
+        self.send_to_server(message)
+        response = json.loads(self.socket.recv(1024))
+        if response.get('error'):
+            log_error(response.get('error'))
+        else:
+            print response
+        pass
 
 
 
 def menu():
-	print "\nChoose an option: "
-	print "1 - CREATE message box"
-	print "2 - LIST message boxes"
-	print "3 - List NEW messages in message box"
-	print "4 - List ALL messages in message box"
-	print "5 - SEND a message to a message box"
-	print "6 - RECEIV a specific message from a message box"
-	print "7 - Send a message RECEIPT"
-	print "8 - Check for message reception STATUS"
-	print "0 - EXIT"
+    print "\nChoose an option: "
+    print "1 - CREATE message box"
+    print "2 - LIST message boxes"
+    print "3 - List NEW messages in message box"
+    print "4 - List ALL messages in message box"
+    print "5 - SEND a message to a message box"
+    print "6 - RECEIV a specific message from a message box"
+    print "7 - Send a message RECEIPT"
+    print "8 - Check for message reception STATUS"
+    print "0 - EXIT"
 
 
 if __name__ == "__main__":
 
-	try:
-		client = Client(sys.argv[1])
-	except IndexError:
-		log_error("Please pass a uuid as argument")
-		sys.exit(-1)
+    try:
+        client = Client(sys.argv[1])
+    except IndexError:
+        log_error("Please pass a uuid as argument")
+        sys.exit(-1)
 
 
 
-	while 1:
-		menu()
-		x = None
-
-		try:
-			x = int(input("Opt: "))
-		except Exception as e:
-			print("Must be an integer")
-
-
-		if x == 1:
-			client.Create()
-
-		elif x == 2:
-			uid = get_int(question = "User ID? ")
-			if uid == None:
-				client.List()
-			else:
-				client.List(uid)
-
-		elif x == 3:
-			result = get_int(question = "User ID? ")
-			client.New(result) if result else log_error("Invalid Value")
-
-		elif x == 4:
-			result = get_int(question = "User ID? ")
-			client.All(result) if result else log_error("Invalid Value")
-
-		elif x == 5:  # Send Function
-
-			dst = get_int(question = "Destination User ID? ")
-			
-			msg = str(raw_input("Message? "))
-
-			if dst == None:
-				log_error("Invalid Destination ID")
-
-			if msg == None:
-				log_error("Invalid Message")
+    while 1:
+        menu()
+        x = None
+        try:
+            x = int(input("Opt: "))
+        except Exception as e:
+            print("Must be an integer")
 
 
-			if dst == None or msg == None:
-				continue
-			else:
-				client.Send(dst, msg)
+        if x == 1:
+            client.Create()
+
+        elif x == 2:
+            uid = get_int(question = "User ID? ")
+            client.List(uid)
+
+        elif x == 3:
+            result = get_int(question = "User ID? ")
+            client.New(result) if result else log_error("Invalid Value")
+
+        elif x == 4:
+            result = get_int(question = "User ID? ")
+            client.All(result) if result else log_error("Invalid Value")
+
+        elif x == 5:  # Send Function
+
+            dst = get_int(question = "Destination User ID? ")
+
+            msg = str(raw_input("Message? "))
+
+            if dst == None:
+                log_error("Invalid Destination ID")
+
+            if msg == None:
+                log_error("Invalid Message")
+            if dst == None or msg == None:
+                continue
+            else:
+                client.Send(dst, msg)
 
 
-		elif x == 6:
-			receiver = get_int(question = "Receiver User ID? ")
-			if receiver == None:
-				log_error("Invalid Value")
+        elif x == 6:
+            receiver = get_int(question = "Receiver User ID? ")
+            if receiver == None:
+                log_error("Invalid Value")
 
-			sender = get_int(question = "Sender User ID? ")
-			if sender == None:
-				log_error("Invalid Value")
-						
-			boxId = get_int(question = "Message ID? ")
-			if boxId == None:
-				log_error("Invalid Value")
+            sender = get_int(question = "Sender User ID? ")
+            if sender == None:
+                log_error("Invalid Value")
 
-			box = str(sender) + "_" + str(boxId)
-			client.Recv(receiver, box)
+            boxId = get_int(question = "Message ID? ")
+            if boxId == None:
+                log_error("Invalid Value")
 
-
-		elif x == 7:
-			sender = get_int(question = "Sender User ID? ")
-			if sender == None:
-				log_error("Invalid Value")
-						
-			boxId = get_int(question = "Message ID? ")
-			if boxId == None:
-				log_error("Invalid Value")
+            box = str(sender) + "_" + str(boxId)
+            client.Recv(receiver, box)
 
 
-			box = str(sender) + "_" + str(boxId)
-			client.Receipt(box)
+        elif x == 7:
+            sender = get_int(question = "Sender User ID? ")
+            if sender == None:
+                log_error("Invalid Value")
 
-		elif x == 8:
-			sender = get_int(question = "Sender User ID? ")
-			if sender == None:
-				log_error("Invalid Value")
+            boxId = get_int(question = "Message ID? ")
+            if boxId == None:
+                log_error("Invalid Value")
 
-			receiver = get_int(question = "Receiver User ID? ")
-			if receiver == None:
-				log_error("Invalid Value")
-						
-			boxId = get_int(question = "Message ID? ")
-			if boxId == None:
-				log_error("Invalid Value")
 
-			box = str(receiver) + "_" + str(boxId)
-			client.Status(sender, box)
+            box = str(sender) + "_" + str(boxId)
+            client.Receipt(box)
 
-		elif x == 0:
-			break
+        elif x == 8:
+            sender = get_int(question = "Sender User ID? ")
+            if sender == None:
+                log_error("Invalid Value")
 
-		else:
-			continue
+            receiver = get_int(question = "Receiver User ID? ")
+            if receiver == None:
+                log_error("Invalid Value")
+
+            boxId = get_int(question = "Message ID? ")
+            if boxId == None:
+                log_error("Invalid Value")
+
+            box = str(receiver) + "_" + str(boxId)
+            client.Status(sender, box)
+
+        elif x == 0:
+            break
+
+        else:
+            continue
 
 
 
-	client.socket.close()
-	print "byee :) "
+    client.socket.close()
+    print "byee :) "
 
 
